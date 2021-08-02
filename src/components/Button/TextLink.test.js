e3cb9480e5959de6ed9491ee3b3e66bf1732c770
jungwoo <westhyena@gmail.com>
2021-08-02 23:04:07 +0900

    test(TextLink): add test for TextLink

---
+++ src/components/Button/TextLink.test.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import each from 'jest-each'
+import { mount } from 'enzyme'
+
+import TextLink from 'components/Button/TextLink'
+import cls from 'helpers/class'
+
+
+const Text = 'Link Text'
+
+describe('default', () => {
+  it('text', () => {
+    const wrapper = mount(<TextLink>{Text}</TextLink>)
+    expect(wrapper.text()).toBe(Text)
+  })
+})
+
+describe('underline', () => {
+  it('underline', () => {
+    const wrapper = mount(
+      <TextLink underline>{Text}</TextLink>
+    )
+    expect(wrapper.text()).toBe(Text)
+    expect(wrapper.find(`.${cls('textlink', 'underline')}`)).toHaveLength(2)
+  })
+})
+
+describe('disabled', () => {
+  it('disabled', () => {
+    const wrapper = mount(
+      <TextLink disabled>{Text}</TextLink>
+    )
+    expect(wrapper.text()).toBe(Text)
+    expect(wrapper.find(`.${cls('textlink', 'disabled')}`)).toHaveLength(2)
+  })
+})
+
+describe('font weight', () => {
+  it('font bold', () => {
+    const wrapper = mount(
+      <TextLink bold>{Text}</TextLink>
+    )
+    expect(wrapper.text()).toBe(Text)
+    expect(wrapper.childAt(0).props().style).toHaveProperty('fontWeight', 'bold')
+  })
+  it('font normal', () => {
+    const wrapper = mount(
+      <TextLink bold={false}>{Text}</TextLink>
+    )
+    expect(wrapper.text()).toBe(Text)
+    expect(wrapper.childAt(0).props().style).toHaveProperty('fontWeight', 'normal')
+  })
+})
