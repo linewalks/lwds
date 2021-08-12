8d7de542c1972184ea9b6c38b666b2d5160d0666
jungwoo <westhyena@gmail.com>
2021-08-12 23:48:20 +0900

    test(Dropdown): add test dropdown

---
+++ src/components/Menu/Dropdown/Dropdown.test.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import each from 'jest-each'
+import { mount } from 'enzyme'
+
+import Dropdown from '@components/Menu/Dropdown/Dropdown'
+import DropdownItem from '@components/Menu/Dropdown/Item'
+import DropdownDivider from '@components/Menu/Dropdown/Divider'
+import cls from '@helpers/class'
+
+describe('size', () => {
+  each([
+    'lg',
+    'md'
+  ]).it('%s', (size) => {
+    const wrapper = mount(
+      <Dropdown size={size}>
+        <DropdownItem>item</DropdownItem>
+        <DropdownDivider />
+        <DropdownItem>item</DropdownItem>
+      </Dropdown>
+    )
+    expect(wrapper.find(`.${cls('dropdown', size)}`)).toHaveLength(1)
+    expect(wrapper.find(`.${cls('dropdown', 'item', size)}`)).toHaveLength(2)
+  })
+})
+
+describe('item click', () => {
+  it('button click', () => {
+    const handleClick = jest.fn()
+    const wrapper = mount(
+      <Dropdown>
+        <DropdownItem isButton onClick={handleClick}>item</DropdownItem>
+      </Dropdown>
+    )
+    wrapper.find(DropdownItem).simulate('click')
+    expect(handleClick).toHaveBeenCalled()
+  })
+})
+
+describe('bold', () => {
+  it('not bold', () => {
+    const wrapper = mount(
+      <Dropdown>
+        <DropdownItem>item</DropdownItem>
+      </Dropdown>
+    )
+    expect(wrapper.find(DropdownItem).childAt(0).props().style.fontWeight).toBe('normal')
+  })
+  it('bold', () => {
+    const wrapper = mount(
+      <Dropdown>
+        <DropdownItem bold>item</DropdownItem>
+      </Dropdown>
+    )
+    expect(wrapper.find(DropdownItem).childAt(0).props().style.fontWeight).toBe('bold')
+  })
+})
