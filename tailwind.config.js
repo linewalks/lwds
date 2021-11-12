e5ea741b08a97547cdd0f9fc7ba8cc3ae7cfd335
goodlana <lahee13579@gmail.com>
2021-11-12 18:27:16 +0900

    improvement: tailwind.config.js 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,13 @@
+const { colors, spacing, size, boxShadow } = require('./tailwindConfigConverter.js')
+
 module.exports = {
   purge: {},
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors,
+    spacing,
+    size,
+    boxShadow,
     extend: {},
   },
   variants: {
