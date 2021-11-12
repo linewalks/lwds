e2104d8b9770635df0f4e63889ace1506ceb1a88
goodlana <lahee13579@gmail.com>
2021-11-12 18:25:22 +0900

    improvement: size.js, spacing.js 생성

---
+++ src/assets/styles/size.js
@@ -0,0 +1,13 @@
+//tailwind.config.js 설정을 위해 CommonJS 방식 사용
+module.exports = {
+  $xs: '16px',
+  $sm: '24px',
+  $md: '32px',
+  $lg: '40px',
+  $xl: '56px',
+  '$xs-r': '1rem',
+  '$sm-r': '1.5rem',
+  '$md-r': '2rem',
+  '$lg-r': '2.5rem',
+  '$xl-r': '3rem',
+}
