33903b226b20624376f614c6a2bd47a71ff6f830
markleemee <markleemee@gmail.com>
2021-11-17 14:24:29 +0900

    feat: width, responsive, block 추가

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -16,10 +16,14 @@ interface ButtonProps {
   disabled: boolean
   size: string | 'sm' | 'md' | 'lg' | 'xlg'
   // TODO variant => variant와 color로 나누기
-  variant: (
-    string | 'primary' | 'primary_line' | 'basic' |
-    'basic_line' | 'primary_light' | 'basic_light'
-  )
+  variant:
+    | string
+    | 'primary'
+    | 'primary_line'
+    | 'basic'
+    | 'basic_line'
+    | 'primary_light'
+    | 'basic_light'
   style?: object
   ghostText: string
   isResponsive: boolean
@@ -30,63 +34,73 @@ interface ButtonProps {
   rightIcon?: React.ReactElement
   children: React.ReactNode
   onClick?: React.MouseEventHandler<HTMLElement>
+  width: number | string | 'defalut' | '100%'
 }
 
-const Button = React.forwardRef((props: ButtonProps, ref: React.RefObject<any>) => {
-  const {
-    id,
-    disabled,
-    size,
-    variant,
-    style,
-    bold,
-    block,
-    leftIcon,
-    rightIcon,
-    children,
-    onClick,
-    ghostType,
-    ...rest
-  } = props
+const Button = React.forwardRef(
+  (props: ButtonProps, ref: React.RefObject<any>) => {
+    const {
+      id,
+      disabled,
+      size,
+      variant,
+      style,
+      bold,
+      block,
+      leftIcon,
+      rightIcon,
+      children,
+      onClick,
+      ghostType,
+      width,
+      isResponsive,
+      ...rest
+    } = props
 
-  const createIconNode = (icon: React.ReactElement, className: string) => {
-    return icon && React.cloneElement(icon, {
-      className: cls('button', className),
-      width: '1em',
-      height: '1em'
-    })
-  }
+    const createIconNode = (icon: React.ReactElement, className: string) => {
+      return (
+        icon &&
+        React.cloneElement(icon, {
+          className: cls('button', className),
+          width: '1em',
+          height: '1em',
+        })
+      )
+    }
 
-  const startIconNode = createIconNode(leftIcon, 'lefticon')
-  const endIconNode = createIconNode(rightIcon, 'righticon')
+    const startIconNode = createIconNode(leftIcon, 'lefticon')
+    const endIconNode = createIconNode(rightIcon, 'righticon')
 
-  return (
-    <button
-      {...rest}
-      id={id}
-      disabled={disabled}
-      className={clsx(
-        cls('button'),
-        cls('button', size),
-        cls('button', variant),
-        block && cls('button', 'block'),
-        block && size === 'sm' && cls('button', 'block', size)
-      )}
-      style={{
-        ...style,
-        fontWeight: bold ? 'bold' : 'normal'
-      }}
-      onClick={onClick}
-      ref={ref}
-    >
-      <WrapInside>
-        {startIconNode}
-        {children}
-        {endIconNode}
-      </WrapInside>
-    </button>
-  )
-})
+    return (
+      <button
+        {...rest}
+        id={id}
+        disabled={disabled}
+        className={clsx(
+          cls('button'),
+          cls('button', size),
+          cls('button', variant),
+          block && cls('button', 'block'),
+          block && size === 'sm' && cls('button', 'block', size),
+          isResponsive && cls('button', 'responsive', size),
+        )}
+        style={{
+          ...style,
+          width: width,
+          fontWeight: bold ? 'bold' : 'normal',
+        }}
+        onClick={onClick}
+        ref={ref}
+      >
+        <WrapInside>
+          {startIconNode}
+          {children}
+          {endIconNode}
+        </WrapInside>
+      </button>
+    )
+  },
+)
 
 Button.defaultProps = {
   disabled: false,
@@ -96,6 +110,7 @@ Button.defaultProps = {
   block: false,
   isResponsive: false,
   ghostType: 'default',
+  width: 'defalut',
 }
 
 export default Button
