644b9d0f08cf15ade158f3c4eb0ecff24c2625b7
markleemee <markleemee@gmail.com>
2021-12-14 11:46:21 +0900

    build: error 수정본 build

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,8 @@
 import Button from "./components/Button/Button";
-export { Button };
+import colors from "./assets/styles/color/colors";
+import semantics from "./assets/styles/color/semantics";
+import shadow from "./assets/styles/shadow";
+import size from "./assets/styles/size";
+import spacing from "./assets/styles/spacing";
+export { Button, colors, semantics, shadow, size, spacing };
 //# sourceMappingURL=index.d.ts.map
