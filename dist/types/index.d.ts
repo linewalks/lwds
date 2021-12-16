bed16cd198cf263b227d77cd19712a870ae0357f
edit8080 <leetaehee0205@gmail.com>
2021-12-16 11:25:45 +0900

    build : new version build

--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -2,7 +2,7 @@ import Button from '@components/Button/Button';
 import colors from '@assets/styles/color/colors';
 import semantics from '@assets/styles/color/semantics';
 import shadow from '@assets/styles/shadow';
-import size from '@assets/styles/size';
-import spacing from '@assets/styles/spacing';
+import size from '@assets/styles/size/semantics';
+import spacing from '@assets/styles/spacing/semantics';
 export { Button, colors, semantics, shadow, size, spacing, };
 //# sourceMappingURL=index.d.ts.map
