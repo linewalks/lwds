944a6ed328d9bdd0d52c7b7fba4e0336c169ec80
edit8080 <leetaehee0205@gmail.com>
2021-12-16 11:26:46 +0900

    docs : update styleguide docs

---
+++ docs/styles/size/size.js
@@ -0,0 +1,32 @@
+//tailwind.config.js 설정을 위해 CommonJS 방식 사용
+module.exports = {
+  // px
+  p0: '0px',
+  p01: '8px',
+  p02: '16px',
+  p03: '20px',
+  p04: '24px',
+  p05: '32px',
+  p06: '40px',
+  p07: '48px',
+  p08: '56px',
+  p09: '64px',
+  p10: '72px',
+  p11: '80px',
+  p12: '88px',
+  p13: '96px',
+  // rem
+  r01: '0.5rem',
+  r02: '1rem',
+  r03: '1.25rem',
+  r04: '1.5rem',
+  r05: '2rem',
+  r06: '2.5rem',
+  r07: '3rem',
+  r08: '3.5rem',
+  r09: '4rem',
+  r10: '4.5rem',
+  r11: '5rem',
+  r12: '5.5rem',
+  r13: '6rem',
+}
