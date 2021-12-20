2d00b210f01aad75ff462ea8f932cc24a76edd18
goodlana <lahee13579@gmail.com>
2021-12-20 19:14:39 +0900

    styles: font.scss - fontSize, fontColor 추가

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 import Button from '@components/Button/Button'
-import colors from '@assets/styles/color/colors'
-import semantics from '@assets/styles/color/semantics'
-import shadow from '@assets/styles/shadow'
-import size from '@assets/styles/size/semantics'
-import spacing from '@assets/styles/spacing/semantics'
+import styles from '@assets/styles'
+// import semantics from '@assets/styles/color/semantics'
+// import shadow from '@assets/styles/shadow'
+// import size from '@assets/styles/size/semantics'
+// import spacing from '@assets/styles/spacing/semantics'
 
 // import TextLink from '@components/Button/TextLink'
 
@@ -31,11 +31,7 @@ import spacing from '@assets/styles/spacing/semantics'
 
 export {
   Button,
-  colors,
-  semantics,
-  shadow,
-  size,
-  spacing,
+  styles,
   // Checkbox,
   // Radio,
   // RadioGroup,
