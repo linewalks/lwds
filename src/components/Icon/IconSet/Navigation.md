ab0a5259c4ae43c7ae75ff53fb4255d3a986c3da
edit8080 <leetaehee0205@gmail.com>
2021-12-22 10:51:43 +0900

    docs(icon): create Navigation icon set docs

---
+++ src/components/Icon/IconSet/Navigation.md
@@ -0,0 +1,10 @@
+#### Icon
+
+```js
+import Navigation from '@components/Icon/IconSet/Navigation'
+
+const Default = () => {
+  return <Navigation />
+}
+Default()
+```
---
+++ src/components/Icon/IconSet/Navigation.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import _ from 'lodash'
+import styled from 'styled-components'
+
+import * as NavigationIcons from '@components/Icon/Icons/Navigation'
+
+const WrapIcons = styled.div`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-gap: 10px;
+`
+
+const WrapIcon = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+  background-color: #eeeeee;
+  border-radius: 10px;
+`
+
+const Navigation = () => {
+  const iconNames = _.keys(NavigationIcons)
+
+  return (
+    <>
+      <WrapIcons>
+        {_.map(_.values(NavigationIcons), (Icon, idx) => (
+          <WrapIcon>
+            <div style={{ margin: '5px 0 10px' }}>
+              <Icon />
+            </div>
+            {iconNames[idx]}
+          </WrapIcon>
+        ))}
+      </WrapIcons>
+    </>
+  )
+}
+
+export default Navigation
