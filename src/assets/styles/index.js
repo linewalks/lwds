0b7b827919e28d6c9984af129051f1a2ab5d118f
edit8080 <leetaehee0205@gmail.com>
2021-12-30 15:14:53 +0900

    improvement: remove dollars in styles semantics

--- src/assets/styles/index.js
+++ src/assets/styles/index.js
@@ -3,9 +3,19 @@ const sizeSem = require('./size/semantics')
 const spacing = require('./spacing/semantics')
 const shadow = require('./shadow/semantics')
 
+// lodash 사용시 불필요한 모듈도 export 되어 js 내에서 해결
+const deleteDollar = (obj) => {
+  let ret = {}
+
+  Object.keys(obj).map((key) =>
+    Object.assign(ret, { [key.substring(1)]: obj[`${key}`] }),
+  )
+  return ret
+}
+
 module.exports = {
-  ...colors,
-  ...sizeSem.size,
-  ...spacing,
+  ...deleteDollar(colors),
+  ...deleteDollar(sizeSem.size),
+  ...deleteDollar(spacing),
   ...shadow,
 }
