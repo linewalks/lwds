64647ce069b841ab937aebd22dc1696d988c313a
edit8080 <leetaehee0205@gmail.com>
2022-02-07 14:40:14 +0900

    feat(shadow): create shadow semantics

--- src/assets/styles/shadow/semantics.js
+++ src/assets/styles/shadow/semantics.js
@@ -5,9 +5,4 @@ module.exports = {
   $shadow_2: shadow.$shadow_02,
   $shadow_3: shadow.$shadow_03,
   $shadow_4: shadow.$shadow_04,
-  $shadow_5: shadow.$shadow_05,
-  $shadow_6: shadow.$shadow_06,
-  $shadow_hover: shadow.hover,
-  $shadow_active: shadow.active,
-  $shadow_dimmed: shadow.dimmed,
 }
