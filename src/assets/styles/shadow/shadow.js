375d46887731cd7e87f7ca084e98d0ff0ae49aae
edit8080 <leetaehee0205@gmail.com>
2022-02-08 09:36:30 +0900

    feat(shadow): split shadow color semantics

--- src/assets/styles/shadow/shadow.js
+++ src/assets/styles/shadow/shadow.js
@@ -1,7 +1,9 @@
+const colors = require('../color/colors')
+
 //tailwind.config.js 설정을 위해 CommonJS 방식 사용
 module.exports = {
-  $shadow_01: '0 0 4px 0 rgba(36, 41, 45, 0.28)',
-  $shadow_02: '0 1px 8px 0 rgba(36, 41, 45, 0.28)',
-  $shadow_03: '0 2px 14px 0 rgba(36, 41, 45, 0.24)',
-  $shadow_04: '0 3px 24px 0 rgba(36, 41, 45, 0.24)',
+  $shadow_01: `0 0 4px 0 ${colors.lw_shadow_01}`,
+  $shadow_02: `0 1px 8px 0 ${colors.lw_shadow_01}`,
+  $shadow_03: `0 2px 14px 0 ${colors.lw_shadow_02}`,
+  $shadow_04: `0 3px 24px 0 ${colors.lw_shadow_02}`,
 }
