67d5a2ba27727c8fab471e9c434e53c5f7c07f4e
edit8080 <leetaehee0205@gmail.com>
2022-02-09 10:19:03 +0900

    fix: button scss shadow style error

--- src/assets/styles/shadow/shadow.js
+++ src/assets/styles/shadow/shadow.js
@@ -1,9 +1,15 @@
+// tailwindCSS 에서 사용하기 위한 용도
 const colors = require('../color/colors')
 
-//tailwind.config.js 설정을 위해 CommonJS 방식 사용
+// tailwind.config.js 설정을 위해 CommonJS 방식 사용
 module.exports = {
   $shadow_v1: `0 0 4px 0 ${colors.lw_shadow_01}`,
   $shadow_v2: `0 1px 8px 0 ${colors.lw_shadow_01}`,
   $shadow_v3: `0 2px 14px 0 ${colors.lw_shadow_02}`,
   $shadow_v4: `0 3px 24px 0 ${colors.lw_shadow_02}`,
+
+  // hover, active, dimmed는 기존 background 위에 덮어씌워야 하기 때문에 inset shadow로 설정
+  $shadow_hover: `inset 5rem 5rem ${colors.$lw_hover_overlay}`,
+  $shadow_active: `inset 5rem 5rem ${colors.$lw_active_overlay}`,
+  $shadow_dimmed: `inset 5rem 5rem ${colors.$lw_dimmed_overlay}`,
 }
