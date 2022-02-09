a529061bdaafb064120d31d0cdb182ff54253d4b
edit8080 <leetaehee0205@gmail.com>
2022-02-09 12:19:20 +0900

    chore: use shadow color semantics

--- src/assets/styles/shadow/shadow.js
+++ src/assets/styles/shadow/shadow.js
@@ -3,10 +3,10 @@ const colors = require('../color/colors')
 
 // tailwind.config.js 설정을 위해 CommonJS 방식 사용
 module.exports = {
-  $shadow_v1: `0 0 4px 0 ${colors.lw_shadow_01}`,
-  $shadow_v2: `0 1px 8px 0 ${colors.lw_shadow_01}`,
-  $shadow_v3: `0 2px 14px 0 ${colors.lw_shadow_02}`,
-  $shadow_v4: `0 3px 24px 0 ${colors.lw_shadow_02}`,
+  $shadow_v1: `0 0 4px 0 ${colors.$lw_shadow_01}`,
+  $shadow_v2: `0 1px 8px 0 ${colors.$lw_shadow_01}`,
+  $shadow_v3: `0 2px 14px 0 ${colors.$lw_shadow_02}`,
+  $shadow_v4: `0 3px 24px 0 ${colors.$lw_shadow_02}`,
 
   // hover, active, dimmed는 기존 background 위에 덮어씌워야 하기 때문에 inset shadow로 설정
   $shadow_hover: `inset 5rem 5rem ${colors.$lw_hover_overlay}`,
