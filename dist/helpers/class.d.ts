e349841bf213c651e5a84a9b1d1115c2031802df
Seonyeong Jang <ssoju2827@gmail.com>
2022-03-03 13:59:30 +0900

    chore: delete dist

--- dist/helpers/class.d.ts
+++
@@ -1,3 +0,0 @@
-declare const getCls: (...args: string[]) => string;
-export default getCls;
-//# sourceMappingURL=class.d.ts.map
--- dist/icons/components/Icon/Icons/Specific/ProjectSelectedIcon.d.ts
+++
@@ -1,4 +0,0 @@
-import { SVGIconProps } from '@components/Icon/Icons/SVGIcon';
-declare const ProjectSelectedIcon: (props: SVGIconProps) => JSX.Element;
-export default ProjectSelectedIcon;
-//# sourceMappingURL=ProjectSelectedIcon.d.ts.map
