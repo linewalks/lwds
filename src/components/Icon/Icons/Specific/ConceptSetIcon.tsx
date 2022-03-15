6f7a6be26b47e1c00da1004443b3f58e8e225b60
edit8080 <leetaehee0205@gmail.com>
2022-03-15 12:52:22 +0900

    fix: update concept set icon

--- src/components/Icon/Icons/Specific/ConceptSetIcon.tsx
+++ src/components/Icon/Icons/Specific/ConceptSetIcon.tsx
@@ -11,7 +11,7 @@ const ConceptSetIcon = (props: SVGIconProps) => {
           y1="41.621%"
           x2="-8.379%"
           y2="41.621%"
-          id="96whp9apva"
+          id="4amzy1vzca"
         >
           <stop stopColor="#848F9A" offset="0%" />
           <stop stopColor="#4E5762" offset="66.259%" />
@@ -22,7 +22,7 @@ const ConceptSetIcon = (props: SVGIconProps) => {
           y1="-16.759%"
           x2="50%"
           y2="100%"
-          id="25j2332z2b"
+          id="p41l8hzhcb"
         >
           <stop stopColor="#848F9A" offset="0%" />
           <stop stopColor="#4E5762" offset="66.259%" />
@@ -33,22 +33,21 @@ const ConceptSetIcon = (props: SVGIconProps) => {
           y1="38.983%"
           x2="136.568%"
           y2="39.784%"
-          id="fzyky4csdc"
+          id="9sozix2ypc"
         >
           <stop stopColor="#96A0A9" offset="0%" />
           <stop stopColor="#B3BBC2" offset="48.569%" />
           <stop stopColor="#FFF" offset="100%" />
         </linearGradient>
         <linearGradient
-          x1="22.655%"
-          y1="56.378%"
+          x1="22.658%"
+          y1="50%"
           x2="22.658%"
-          y2="38.285%"
-          id="joqx3vufef"
+          y2="-41.848%"
+          id="9t99sguoef"
         >
           <stop stopColor="#96A0A9" offset="0%" />
-          <stop stopColor="#B3BBC2" offset="34.205%" />
-          <stop stopColor="#FFF" offset="100%" />
+          <stop stopColor="#BEBEBE" offset="100%" />
         </linearGradient>
         <filter
           x="-7.1%"
@@ -56,34 +55,7 @@ const ConceptSetIcon = (props: SVGIconProps) => {
           width="114.3%"
           height="115.4%"
           filterUnits="objectBoundingBox"
-          id="gbcofuowde"
-        >
-          <feGaussianBlur
-            stdDeviation="1"
-            in="SourceAlpha"
-            result="shadowBlurInner1"
-          />
-          <feOffset in="shadowBlurInner1" result="shadowOffsetInner1" />
-          <feComposite
-            in="shadowOffsetInner1"
-            in2="SourceAlpha"
-            operator="arithmetic"
-            k2="-1"
-            k3="1"
-            result="shadowInnerInner1"
-          />
-          <feColorMatrix
-            values="0 0 0 0 0.300940406 0 0 0 0 0.337563612 0 0 0 0 0.378764719 0 0 0 0.5 0"
-            in="shadowInnerInner1"
-          />
-        </filter>
-        <filter
-          x="-7.1%"
-          y="-20%"
-          width="114.3%"
-          height="140%"
-          filterUnits="objectBoundingBox"
-          id="egswzew00h"
+          id="xzw0f13see"
         >
           <feGaussianBlur
             stdDeviation="1"
@@ -106,11 +78,7 @@ const ConceptSetIcon = (props: SVGIconProps) => {
         </filter>
         <path
           d="M0 2.5h14v10.406c0 1.433-3.134 2.594-7 2.594-3.77 0-6.843-1.104-6.994-2.487L0 12.906V2.5z"
-          id="fzkmxywxsd"
-        />
-        <path
-          d="M7 0C3.134 0 0 1.12 0 2.5S3.134 5 7 5s7-1.12 7-2.5S10.866 0 7 0z"
-          id="dltkpkqnmg"
+          id="o1fhaosa4d"
         />
       </defs>
       <g fill="none" fillRule="evenodd">
@@ -118,23 +86,24 @@ const ConceptSetIcon = (props: SVGIconProps) => {
         <g fillRule="nonzero">
           <path
             d="M0 2.5h14v10.406c0 1.433-3.134 2.594-7 2.594-3.77 0-6.843-1.104-6.994-2.487L0 12.906V2.5z"
-            fill="url(#96whp9apva)"
+            fill="url(#4amzy1vzca)"
             transform="translate(1 1)"
           />
           <path
             d="M7 0C3.134 0 0 1.12 0 2.5S3.134 5 7 5s7-1.12 7-2.5S10.866 0 7 0z"
-            fill="url(#25j2332z2b)"
+            fill="url(#p41l8hzhcb)"
             transform="translate(1 1)"
           />
         </g>
         <g transform="translate(9 8)">
-          <use fill="url(#fzyky4csdc)" xlinkHref="#fzkmxywxsd" />
-          <use fill="#000" filter="url(#gbcofuowde)" xlinkHref="#fzkmxywxsd" />
+          <use fill="url(#9sozix2ypc)" xlinkHref="#o1fhaosa4d" />
+          <use fill="#000" filter="url(#xzw0f13see)" xlinkHref="#o1fhaosa4d" />
         </g>
-        <g transform="translate(9 8)">
-          <use fill="url(#joqx3vufef)" xlinkHref="#dltkpkqnmg" />
-          <use fill="#000" filter="url(#egswzew00h)" xlinkHref="#dltkpkqnmg" />
-        </g>
+        <path
+          d="M7 0C3.134 0 0 1.12 0 2.5S3.134 5 7 5s7-1.12 7-2.5S10.866 0 7 0z"
+          fill="url(#9t99sguoef)"
+          transform="translate(9 8)"
+        />
       </g>
     </SVGIcon>
   )
