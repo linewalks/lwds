748338f9b8d2b9df5f3f668077afd3a54c9416e7
edit8080 <leetaehee0205@gmail.com>
2022-03-29 14:14:25 +0900

    build(docs): update tag style in github docs

---
+++ dist/types/components/Icon/IconSet/Domain.d.ts
@@ -0,0 +1,17 @@
+import { SVGIconProps } from '@components/Icon/Icons/SVGIcon';
+interface DomainIconProps extends SVGIconProps {
+    rotate?: number;
+}
+declare const _default: {
+    (props: DomainIconProps): JSX.Element;
+    defaultProps: {
+        color: string;
+        width: string;
+        height: string;
+        responsive: boolean;
+        viewBox: string;
+        rotate: number;
+    };
+};
+export default _default;
+//# sourceMappingURL=Domain.d.ts.map
