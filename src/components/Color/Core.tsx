18d529fe254804b7a8037e70c8f507424e7970af
edit8080 <leetaehee0205@gmail.com>
2022-03-29 11:17:23 +0900

    style(docs): add grid style in color docs

--- src/components/Color/Core.tsx
+++ src/components/Color/Core.tsx
@@ -5,9 +5,30 @@ import ColorPalette from '@src/components/Color/Colors'
 const WrapColorPaletteList = styled.section`
   display: grid;
   grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto;
+  grid-template-areas:
+    'palette_1 palette_2'
+    'palette_3 palette_4'
+    'palette_3 palette_5';
   justify-items: center;
   text-align: center;
   row-gap: 40px;
+
+  .color_palette:nth-child(1) {
+    grid-area: palette_1;
+  }
+  .color_palette:nth-child(2) {
+    grid-area: palette_2;
+  }
+  .color_palette:nth-child(3) {
+    grid-area: palette_3;
+  }
+  .color_palette:nth-child(4) {
+    grid-area: palette_4;
+  }
+  .color_palette:nth-child(5) {
+    grid-area: palette_5;
+  }
 `
 
 const CoreColors = () => {
