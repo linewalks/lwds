afda2faaf2da35adcb2349b03f17ea38a23f731b
edit8080 <leetaehee0205@gmail.com>
2022-05-30 18:35:09 +0900

    improvement: filter undefined, false, null

--- src/helpers/class.ts
+++ src/helpers/class.ts
@@ -1,8 +1,8 @@
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import variables from '!!sass-variables-loader!../assets/styles/variables.scss'
 
-const getCls = (...args: string[]): string => {  
-  return [variables.prefix, ...args].join('-')
+const getCls = (...args: string[]): string => {
+  return [variables.prefix, ...args].filter((item) => item ?? false).join('-')
 }
 
 export default getCls
