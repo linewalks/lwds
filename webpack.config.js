6ad468aa7c8d2c84f23d85e7912fe27679828afb
edit8080 <leetaehee0205@gmail.com>
2022-06-28 13:41:33 +0900

    config: add hooks alias path

--- webpack.config.js
+++ webpack.config.js
@@ -69,6 +69,7 @@ module.exports = {
       '@assets': path.resolve(__dirname, 'src/assets/'),
       '@components': path.resolve(__dirname, 'src/components/'),
       '@helpers': path.resolve(__dirname, 'src/helpers/'),
+      '@hooks': path.resolve(__dirname, 'src/hooks/'),
     },
   },
 }
