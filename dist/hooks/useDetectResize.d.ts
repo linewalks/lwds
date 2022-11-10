97f09360961edfd085c354320938728c7f40cd55
rapha.el <ssoju2827@gmail.com>
2022-11-10 15:16:38 +0900

    chore(build): build

---
+++ dist/hooks/useDetectResize.d.ts
@@ -0,0 +1,12 @@
+/**
+ * 현재 window 의 정보 리턴
+ * @returns { innerWidth, innerHeight, outerWidth, outerHeight }
+ */
+declare const useDetectResize: () => {
+    innerWidth: number;
+    innerHeight: number;
+    outerWidth: number;
+    outerHeight: number;
+};
+export default useDetectResize;
+//# sourceMappingURL=useDetectResize.d.ts.map
