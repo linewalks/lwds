895c2fe6f0c243fa22e3ecc717971b245f78cacb
rapha.el <ssoju2827@gmail.com>
2022-11-15 15:05:34 +0900

    chore(build): build

--- docs/styles/shadow/semantics.js
+++ docs/styles/shadow/semantics.js
@@ -1,9 +1,1 @@
-// 외부 모듈 export 용도
-const shadow = require('./shadow')
-
-module.exports = {
-  $shadow_v1: shadow.$shadow_v1,
-  $shadow_v2: shadow.$shadow_v2,
-  $shadow_v3: shadow.$shadow_v3,
-  $shadow_v4: shadow.$shadow_v4,
-}
+const shadow=require("./shadow");module.exports={$shadow_v1:shadow.$shadow_v1,$shadow_v2:shadow.$shadow_v2,$shadow_v3:shadow.$shadow_v3,$shadow_v4:shadow.$shadow_v4};
--- docs/styles/size/btnFieldSize.js
+++ docs/styles/size/btnFieldSize.js
@@ -1,11 +1,1 @@
-//tailwind.config.js 설정을 위해 CommonJS 방식 사용
-module.exports = {
-  sm: '24px',
-  md: '32px',
-  lg: '40px',
-  xl: '56px',
-  sm_r: '1.5rem',
-  md_r: '2rem',
-  lg_r: '2.5rem',
-  xl_r: '3.5rem',
-}
+module.exports={sm:"24px",md:"32px",lg:"40px",xl:"56px",sm_r:"1.5rem",md_r:"2rem",lg_r:"2.5rem",xl_r:"3.5rem"};
