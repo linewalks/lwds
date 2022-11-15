3a6744723e2d2f6dcfc28652ed4fccc56d2912f9
rapha.el <ssoju2827@gmail.com>
2022-11-15 15:05:34 +0900

    chore(build): build

---
+++ dist/icons/components/Icon/Icons/Specific/AddCheckUnaddedIcon.d.ts
@@ -0,0 +1,4 @@
+import { SVGIconProps } from '@components/Icon/Icons/SVGIcon';
+declare const AddCheckUnaddedIcon: (props: SVGIconProps) => JSX.Element;
+export default AddCheckUnaddedIcon;
+//# sourceMappingURL=AddCheckUnaddedIcon.d.ts.map
