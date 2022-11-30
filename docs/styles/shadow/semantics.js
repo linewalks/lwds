a8ceb9fcfcda1293ae26d68d77abe5c7a8028173
Jonghyuk Lee (Mark) <markleemee@gmail.com>
2022-11-30 15:44:52 +0900

    Feat.v0.3.22 (#254)
    * chore(release): 0.3.22
    
    * chore(build): build

--- docs/styles/shadow/semantics.js
+++ docs/styles/shadow/semantics.js
@@ -1,1 +1,9 @@
-const shadow=require("./shadow");module.exports={$shadow_v1:shadow.$shadow_v1,$shadow_v2:shadow.$shadow_v2,$shadow_v3:shadow.$shadow_v3,$shadow_v4:shadow.$shadow_v4};
+// 외부 모듈 export 용도
+const shadow = require('./shadow')
+
+module.exports = {
+  $shadow_v1: shadow.$shadow_v1,
+  $shadow_v2: shadow.$shadow_v2,
+  $shadow_v3: shadow.$shadow_v3,
+  $shadow_v4: shadow.$shadow_v4,
+}
