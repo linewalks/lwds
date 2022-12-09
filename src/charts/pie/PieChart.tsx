a4c8e92e1e26d6222550da552506a277b4ac7fc6
markleemee <markleemee@gmail.com>
2022-12-09 14:25:36 +0900

    feat: pie chart arch

---
+++ src/charts/pie/PieChart.tsx
@@ -0,0 +1,68 @@
+import React, { useEffect } from 'react'
+import _ from 'lodash'
+import * as d3 from 'd3'
+import styled from 'styled-components'
+
+import Legend from './Legend'
+
+const PieChart = ({
+  // props 논의 필요. 임의 지정.
+  data,
+  colors,
+  style,
+  duration,
+  tooltip,
+  id = _.uniqueId('pie_chart'),
+}) => {
+  return (
+    <WrapChartBox id={id}>
+      <WrapSvg className="svg" />
+      <Legend data={data} colors={colors} />
+      <WrapTooltip className="tooltip-box">
+        <WrapSircle className="tooltip-sircle" />
+        <WrapTextBox className="tooltip-text-box" />
+      </WrapTooltip>
+    </WrapChartBox>
+  )
+}
+
+const WrapChartBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+`
+
+const WrapSvg = styled.svg`
+  width: 100%;
+  height: 100%;
+`
+
+const WrapTooltip = styled.div`
+  visibility: hidden;
+  display: flex;
+  align-items: center;
+  position: absolute;
+  min-width: 5rem;
+  width: max-content;
+  padding: 12px;
+  background-color: rgba(255, 255, 255, 0.95);
+  box-shadow: 0 3px 24px 0 rgba(36, 41, 45, 0.24);
+  border-radius: 0.3rem;
+  opacity: 1;
+`
+
+const WrapSircle = styled.div`
+  width: 0.5rem;
+  height: 0.5rem;
+  margin-right: 8px;
+  border-radius: 50%;
+`
+
+const WrapTextBox = styled.div`
+  display: flex;
+  width: 100%;
+  font-size: 1rem;
+`
+
+export default PieChart
